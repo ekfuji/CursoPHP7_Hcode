28a874d3566a2b3ceefcc4e0cc22e9e13cb0d0a5
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-07-21 17:37:05 -0300

    Variáveis

---
+++ variaveis/exemplo-01.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome = "Keikoramica";
+
+//printar conteúdo da variável utilizamos echo
+//echo $nome;
+//verificar tipo de dado, o tamanho em espaço que ele ocupou e o valor
+var_dump($nome);
+
+
+
+?>
