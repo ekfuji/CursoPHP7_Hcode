810fb28a6e7043f67affd2348bb41c17044c9065
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-07-22 20:45:51 -0300

    Métodos mágicos (__construct, __destruct, __toString, unset)

---
+++ POO/exemplo-04.php
@@ -0,0 +1,36 @@
+<?php 
+
+class Endereco
+{
+	private $logradouro;
+	private $numero;
+	private $cidade;
+
+	public function __construct($l, $n, $c)
+	{
+		$this->logradouro = $l;
+		$this->numero = $n;
+		$this->cidade = $c;
+	}
+	public function __destruct()
+	{
+		var_dump("DESTRUIR");
+	}
+
+	public function __toString()
+	{
+		return $this->logradouro.", ".$this->numero.", ".$this->cidade. "<br>";
+	}
+
+}
+
+$meuEndereco = new Endereco("Rua Exemplo", "123", "Exemplo");
+
+//var_dump($meuEndereco);
+echo $meuEndereco;
+
+//unset($meuEndereco);
+
+
+
+ ?>
