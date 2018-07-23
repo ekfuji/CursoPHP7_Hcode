211507c6f0f056c8be4137a88fbb3effd4c30d22
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-07-23 03:40:39 -0300

    Mysqli

---
+++ mysqli/exemplo-01.php
@@ -0,0 +1,16 @@
+<?php 
+
+$conn = new mysqli("localhost", "root", "");
+
+if ($conn->connect_error)
+{
+	echo "Error:" . $conn->connect_error;
+}
+
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, desenha) VALUES (? , ?)";
+$stmt->bind_param("ss", $login, $pass );
+$login = "user";
+$pass = "12345";
+$stmt->execute();
+
+ ?>
